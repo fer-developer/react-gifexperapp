164aab4d5336e7dfd55751e227663f311016b426
Fernando Mendoza <fernando.mendoza.developer@gmail.com>
2020-09-04 11:26:39 -0500

    Docs Agregado

---
+++ docs/precache-manifest.b7afded2365cd6d97bbc804e099a5e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25c3e1b2f351eaf9d2e2ddd45716357",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3e4a14884fb78abcda2",
+    "url": "/static/css/main.08a6518e.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e4a14884fb78abcda2",
+    "url": "/static/js/main.01aaafca.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
